f2a4988da893ef81fec165103daaece838f24802
Mark Thebault <markthebaultpro@gmail.com>
2020-01-15 17:46:56 +0100

    add new chart

--- add-chart.sh
+++ add-chart.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname "$0")
 echo "USAGE $0 [Path to the chart]"
 
 if helm package $1 -d $BASEDIR ; then
-	helm repo index .
+	cd $BASEDIR && helm repo index .
 	cd $BASEDIR && git add -A && git commit -m "add new chart" && git push
 else
 	echo "Error chart not pushed"
